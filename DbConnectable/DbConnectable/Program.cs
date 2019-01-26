c731bc5876d26347e08c8d3058683d1302ae5c7d
mirceta <kristijan.mirceta@gmail.com>
2019-01-26 20:43:02 +0100

    Added DbConnectable quick check tool

---
+++ DbConnectable/DbConnectable/Program.cs
@@ -0,0 +1,20 @@
+﻿using si.birokrat.next.common.database;
+using System;
+using System.Data;
+
+namespace DbConnectable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = ConnectionString.Format("localhost", "biromaster", "turizem", "q");
+            Connection conn = new Connection(connString);
+            IDbCommand comm = conn.GenerateCommand();
+            comm.CommandType = CommandType.Text;
+            comm.CommandText = "SELECT TOP 3 * FROM plugin_cache";
+            DataTable dt = conn.ExecuteDataTable(comm);
+            
+        }
+    }
+}
